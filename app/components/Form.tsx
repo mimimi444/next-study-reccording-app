d13ca81a9de5197523e62aa0515cab5f04806c4d
mizuki <mizu.etc0720@gmail.com>
2025-10-27 17:08:54 +0900

    モバイル表示時入力欄・結果表示のCSS修正

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -18,12 +18,12 @@ export default function Form({
         <section>
             <form onSubmit={onSubmit}>
                 <div>
-                <label htmlFor="studyContent">学習内容</label>
+                <label htmlFor="studyContent">学習内容:</label>
                 <input type="text" name="studyContent" id="studyCom" value={inputStudyContent} onChange={onChangeContent} placeholder="学習した内容を入力" required/>
                 </div>
                 <div>
-                <label htmlFor="studyTime">学習時間</label>
-                <input type="number" name="studyTime" id="studyTime" min="0" value={inputStudyTime} onChange={onChangeTime} placeholder="" required/>時間
+                <label htmlFor="studyTime">学習時間:</label>
+                <input type="number" name="studyTime" id="studyTime" min="0" value={inputStudyTime} onChange={onChangeTime} placeholder="0" required/>時間
                 </div>
                 <div>入力されている内容:<span>{inputStudyContent}</span></div>
                 <div>入力されている時間:<span>{inputStudyTime}</span></div>
