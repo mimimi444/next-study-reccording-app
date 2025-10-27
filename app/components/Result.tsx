b052db4ecab4e21814d904b7afdc0b9205d8a3b1
mizuki <mizu.etc0720@gmail.com>
2025-10-27 13:37:11 +0900

    アイテム登録後もデフォルトテキストが表示される挙動を修正

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -4,28 +4,26 @@ type Result = {
     sum: number
 }
 
-export default function Result({
-    registeredObj,
-    sum
-}:Result){
+export default function Result({ registeredObj, sum }:Result){
     return(
         <section>
             <h3>ここに登録した内容を表示</h3>
+            {registeredObj.length >1 ? 
             <ul>
                 {registeredObj.map((obj)=>{
-                    if(obj.id!==""){
-                return(
+                    if(obj.id!==""){return(
                     <li key={obj.id}>
                     <div>
                     <p>{obj.content}</p>
                     <p>{obj.time}時間</p>
                     </div>
                     </li>
-                );}else{
-                    return <li key={obj.id}><p>{obj.content}</p></li>
-                }
+                );}
                 })}
             </ul>
+            :<p>学習内容が登録されていません。</p>
+            }
+            
             <div>合計時間：{sum}/1000時間</div> 
         </section>
     )
